56935c3c07be5b6a61fe3177bc913781970888d4
seonghoo1217 <lsh6451217@gmail.com>
2024-06-17 20:50:03 +0900

    #3 feat: 에러 상태 코드를 Enum 타입으로 관리하는 클래스 생성

---
+++ src/main/java/jungle/HandTris/global/exception/ErrorCode.java
@@ -0,0 +1,38 @@
+package jungle.HandTris.global.exception;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.Set;
+
+@Getter
+public enum ErrorCode {
+    // Common
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 생겼습니다. 관리자에게 문의하세요.", Set.of()),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력 값이 올바르지 않습니다.",
+            Set.of(MethodArgumentNotValidException.class, ConstraintViolationException.class)),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다.", Set.of(HttpRequestMethodNotSupportedException.class));
+
+    private final HttpStatusCode status;
+    private final String code;
+    private final String message;
+    private final Set<Class<? extends Exception>> exceptions;
+
+    ErrorCode(HttpStatusCode status, String code, String message, Set<Class<? extends Exception>> exceptions) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+        this.exceptions = exceptions;
+    }
+
+    ErrorCode(HttpStatusCode status, String message, Set<Class<? extends Exception>> exceptions) {
+        this.status = status;
+        this.code = String.valueOf(status.value());
+        this.message = message;
+        this.exceptions = exceptions;
+    }
+}
