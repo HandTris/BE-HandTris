6268fb5983530d1966df5e44666ab2c53a2addc1
seonghoo1217 <lsh6451217@gmail.com>
2024-06-17 16:11:59 +0900

    chore: SpringBoot Application Initialize

---
+++ src/main/java/jungle/HandTris/HandTrisApplication.java
@@ -0,0 +1,13 @@
+package jungle.HandTris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandTrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HandTrisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jungle/HandTris/HandTrisApplicationTests.java
@@ -0,0 +1,13 @@
+package jungle.HandTris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandTrisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
