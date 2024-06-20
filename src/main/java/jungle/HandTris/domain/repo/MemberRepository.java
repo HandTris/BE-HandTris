0c25f3a46bf135a24c92e2151c75657f9eb7b7c4
thun0514 <xogns2301@gmail.com>
2024-06-21 00:27:43 +0900

    feat: nickname 검증을 위한 existsByNickname 추가

--- src/main/java/jungle/HandTris/domain/repo/MemberRepository.java
+++ src/main/java/jungle/HandTris/domain/repo/MemberRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUsername(String username);
     boolean existsByUsername(String username);
+    boolean existsByNickname(String nickname);
 }
