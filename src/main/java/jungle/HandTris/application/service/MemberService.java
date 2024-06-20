92c0ed53dcbd4098755c30c992567d1627774ea6
thun0514 <xogns2301@gmail.com>
2024-06-20 23:05:04 +0900

    feat: 회원가입 API 관련 Service

--- src/main/java/jungle/HandTris/application/service/MemberService.java
+++ src/main/java/jungle/HandTris/application/service/MemberService.java
@@ -4,6 +4,7 @@ import jungle.HandTris.domain.Member;
 import jungle.HandTris.domain.repo.MemberRepository;
 import jungle.HandTris.presentation.dto.request.MemberRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import jakarta.transaction.Transactional;
 
@@ -11,6 +12,7 @@ import jakarta.transaction.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public void signin (MemberRequest memberRequest) {
@@ -18,10 +20,26 @@ public class MemberService {
         String password = memberRequest.getPassword();
 
         Member member = memberRepository.findByUsername(username);
-        // 비밀번호 확인
 
-        if(!member.getPassword().equals(password)){
-            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        // 비밀번호 확인
+        if (!bCryptPasswordEncoder.matches(password, member.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
+    }
+
+    @Transactional
+    public void signup(MemberRequest memberRequest) {
+        boolean isUser = memberRepository.existsByUsername(memberRequest.getUsername());
+        if (isUser) {
+            throw new IllegalArgumentException("이미 존재하는 ID 입니다.");
+        }
+
+        Member data = new Member();
+
+        data.setUsername(memberRequest.getUsername());
+        data.setPassword(bCryptPasswordEncoder.encode(memberRequest.getPassword()));
+        data.setNickname(memberRequest.getNickname());
+
+        memberRepository.save(data);
     }
 }
