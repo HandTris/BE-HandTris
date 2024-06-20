3e99073e1c06496943b2c19f48b858b88c8a5d8e
thun0514 <xogns2301@gmail.com>
2024-06-20 22:05:25 +0900

    feat: 로그인 API 관련 MemberRequest

---
+++ src/main/java/jungle/HandTris/presentation/dto/request/MemberRequest.java
@@ -0,0 +1,11 @@
+package jungle.HandTris.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberRequest {
+    private String username;
+    private String password;
+}
