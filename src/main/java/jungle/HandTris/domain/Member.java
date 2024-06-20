2dc058efd1069854088fbf4c54a841b2619e2ab3
thun0514 <xogns2301@gmail.com>
2024-06-20 23:03:49 +0900

    refactor: Member Entity 정보 수정을 위한 @Setter 추가

--- src/main/java/jungle/HandTris/domain/Member.java
+++ src/main/java/jungle/HandTris/domain/Member.java
@@ -3,9 +3,11 @@ package jungle.HandTris.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
