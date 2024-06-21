dbeb1928bfbcd88a1c4b807bf512dd1a6b282c70
thun0514 <xogns2301@gmail.com>
2024-06-21 14:42:04 +0900

    refactor: OCP원칙을 준수하여 상위 클래스인 Repository 상속

--- src/main/java/jungle/HandTris/domain/repo/MemberRepository.java
+++ src/main/java/jungle/HandTris/domain/repo/MemberRepository.java
@@ -1,9 +1,9 @@
 package jungle.HandTris.domain.repo;
 
 import jungle.HandTris.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends Repository<Member, Long> {
     Member findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
