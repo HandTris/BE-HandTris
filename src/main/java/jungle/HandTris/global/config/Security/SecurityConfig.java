6f7d0ffba5d830b414250a706b053180556216b1
thun0514 <xogns2301@gmail.com>
2024-06-21 21:44:36 +0900

    refactor: 세션 상태를 STATELESS로 설정

--- src/main/java/jungle/HandTris/global/config/Security/SecurityConfig.java
+++ src/main/java/jungle/HandTris/global/config/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -25,6 +26,9 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .csrf((auth) -> auth.disable()
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
 
         return http.build();
