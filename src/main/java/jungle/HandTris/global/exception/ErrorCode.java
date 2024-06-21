30667e8c8775276f0b998d291f974aa84cd29c17
thun0514 <xogns2301@gmail.com>
2024-06-21 20:12:10 +0900

    refactor: 회원 관리 관련 ErrorCode 추가

--- src/main/java/jungle/HandTris/global/exception/ErrorCode.java
+++ src/main/java/jungle/HandTris/global/exception/ErrorCode.java
@@ -1,7 +1,10 @@
 package jungle.HandTris.global.exception;
 
 import jakarta.validation.ConstraintViolationException;
-import jungle.HandTris.domain.exception.CustomException;
+import jungle.HandTris.domain.exception.DuplicateNicknameException;
+import jungle.HandTris.domain.exception.DuplicateUsernameException;
+import jungle.HandTris.domain.exception.PasswordMismatchException;
+import jungle.HandTris.domain.exception.UserNotFoundException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -17,9 +20,10 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력 값이 올바르지 않습니다.",
             Set.of(MethodArgumentNotValidException.class, ConstraintViolationException.class)),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP 메서드입니다.", Set.of(HttpRequestMethodNotSupportedException.class)),
-    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임 입니다.", Set.of(CustomException.class)),
-    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 ID 입니다.", Set.of(CustomException.class)),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "아이디 혹은 비밀번호가 일치하지 않습니다.", Set.of(CustomException.class));
+    DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 닉네임 입니다.", Set.of(DuplicateNicknameException.class)),
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 ID 입니다.", Set.of(DuplicateUsernameException.class)),
+    USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "등록된 사용자가 없습니다.", Set.of(UserNotFoundException.class)),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.", Set.of(PasswordMismatchException.class));
 
     private final HttpStatusCode status;
     private final String code;
