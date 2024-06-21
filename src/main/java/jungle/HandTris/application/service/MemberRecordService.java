22c0197f37446d07a0ac2527d8a0ad4558cd7a33
forrest1398 <129135885+forrest1398@users.noreply.github.com>
2024-06-21 20:21:02 +0900

    refactor : 전적 조회 API를 MemberRecord Layer로 이동

---
+++ src/main/java/jungle/HandTris/application/service/MemberRecordService.java
@@ -0,0 +1,35 @@
+package jungle.HandTris.application.service;
+
+import jakarta.transaction.Transactional;
+import jungle.HandTris.domain.Member;
+import jungle.HandTris.domain.MemberRecord;
+import jungle.HandTris.domain.exception.MemberNotFoundException;
+import jungle.HandTris.domain.exception.MemberRecordNodFoundException;
+import jungle.HandTris.domain.repo.MemberRecordRepository;
+import jungle.HandTris.domain.repo.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberRecordService {
+
+    private final MemberRecordRepository memberRecordRepository;
+    private final MemberRepository memberRepository;
+
+    public MemberRecord getMemberRecord(String memberNickname) {
+
+        Optional<Member> member = memberRepository.findByNickname(memberNickname);
+        if (member.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Optional<MemberRecord> memberRecord = memberRecordRepository.findByMember(member.get());
+        if (memberRecord.isEmpty()) {
+            throw new MemberRecordNodFoundException();
+        }
+        return memberRecord.get();
+    }
+}
--- src/main/java/jungle/HandTris/presentation/controller/MemberController.java
+++ src/main/java/jungle/HandTris/presentation/controller/MemberController.java
@@ -1,12 +1,7 @@
 package jungle.HandTris.presentation.controller;
 
 import jungle.HandTris.application.service.MemberService;
-import jungle.HandTris.domain.MemberRecord;
-import jungle.HandTris.global.dto.ResponseEnvelope;
-import jungle.HandTris.presentation.dto.response.MemberRecordDetailRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
-
     private final MemberService memberService;
-
-    @GetMapping("/{memberNickname}/record")
-    public ResponseEnvelope<MemberRecordDetailRes> getMemberRecord(@PathVariable("memberNickname") String memberNickname) {
-        MemberRecord memberRecord = memberService.getMemberRecord(memberNickname);
-        MemberRecordDetailRes memberRecordDetailRes = new MemberRecordDetailRes(memberRecord);
-        return new ResponseEnvelope<>("200", memberRecordDetailRes, "성공입니다.");
-    }
 }
