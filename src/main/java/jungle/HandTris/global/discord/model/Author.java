17b636d70bf08cdf75b2f983b9329baea11387f1
seonghoo1217 <lsh6451217@gmail.com>
2024-06-24 01:24:06 +0900

    #36 feat: Discord 에러 수집 Model 생성

---
+++ src/main/java/jungle/HandTris/global/discord/model/Author.java
@@ -0,0 +1,5 @@
+package jungle.HandTris.global.discord.model;
+
+public record Author(String name, String url, String iconUrl) {
+
+}
---
+++ src/main/java/jungle/HandTris/global/discord/model/Field.java
@@ -0,0 +1,4 @@
+package jungle.HandTris.global.discord.model;
+
+public record Field(String name, String value, String inline) {
+}
