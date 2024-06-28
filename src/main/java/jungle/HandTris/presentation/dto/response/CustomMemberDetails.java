865ca2990149ffbbd981e57f923e52da69ac4ac2
thun0514 <xogns2301@gmail.com>
2024-06-28 19:50:43 +0900

    feat: Spring Security에서 사용자 정보를 담는 UserDetails 추가

---
+++ src/main/java/jungle/HandTris/presentation/dto/response/CustomMemberDetails.java
@@ -0,0 +1,55 @@
+package jungle.HandTris.presentation.dto.response;
+
+import jungle.HandTris.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomMemberDetails implements UserDetails {
+
+    private final Member memberData;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return memberData.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return memberData.getUsername();
+    }
+
+    public String getNickname() {
+        return memberData.getNickname();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
