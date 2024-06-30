4630db6968a7c20cbb275783b2fbf3c577b80f26
forrest1398 <129135885+forrest1398@users.noreply.github.com>
2024-06-30 18:15:28 +0900

    feat : 전적 갱신 API 구현

--- src/main/java/jungle/HandTris/application/service/MemberRecordService.java
+++ src/main/java/jungle/HandTris/application/service/MemberRecordService.java
@@ -1,7 +1,10 @@
 package jungle.HandTris.application.service;
 
 import jungle.HandTris.domain.MemberRecord;
+import jungle.HandTris.presentation.dto.request.GameResultReq;
 
 public interface MemberRecordService {
     MemberRecord getMemberRecord(String nickname);
+
+    MemberRecord updateMemberRecord(GameResultReq gameResultReq, String nickname);
 }
--- src/main/java/jungle/HandTris/presentation/MemberRecordController.java
+++ src/main/java/jungle/HandTris/presentation/MemberRecordController.java
@@ -1,14 +1,15 @@
 package jungle.HandTris.presentation;
 
+import jakarta.validation.Valid;
 import jungle.HandTris.application.service.MemberRecordService;
 import jungle.HandTris.domain.MemberRecord;
 import jungle.HandTris.global.dto.ResponseEnvelope;
+import jungle.HandTris.global.validation.UserNicknameFromJwt;
+import jungle.HandTris.presentation.dto.request.GameResultReq;
 import jungle.HandTris.presentation.dto.response.MemberRecordDetailRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +21,13 @@ public class MemberRecordController {
     @GetMapping("/{nickname}")
     public ResponseEnvelope<MemberRecordDetailRes> getMemberRecord(@PathVariable("nickname") String nickname) {
         MemberRecord memberRecord = memberRecordService.getMemberRecord(nickname);
+        MemberRecordDetailRes memberRecordDetailRes = new MemberRecordDetailRes(memberRecord);
+        return ResponseEnvelope.of(memberRecordDetailRes);
+    }
+
+    @PutMapping
+    public ResponseEnvelope<MemberRecordDetailRes> updateMemberRecord(@Valid @RequestBody GameResultReq gameResultReq, @UserNicknameFromJwt String nickname) {
+        MemberRecord memberRecord = memberRecordService.updateMemberRecord(gameResultReq, nickname);
         MemberRecordDetailRes memberRecordDetailRes = new MemberRecordDetailRes(memberRecord);
         return ResponseEnvelope.of(memberRecordDetailRes);
     }
