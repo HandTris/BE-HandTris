d3a5903759ba2e10ea9eed3b43531aa673cb40c5
thun0514 <xogns2301@gmail.com>
2024-06-30 21:37:29 +0900

    feat: AWS S3로 업로드 및 URL 반환하는 Service 구현

---
+++ src/main/java/jungle/HandTris/application/impl/S3UploaderServiceImpl.java
@@ -0,0 +1,85 @@
+package jungle.HandTris.application.impl;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import jungle.HandTris.application.service.S3UploaderService;
+import jungle.HandTris.domain.exception.FileConversionException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3UploaderServiceImpl implements S3UploaderService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucketName}")
+    private String bucket;
+
+    // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        System.out.println("public upload");
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new FileConversionException());
+        return upload(uploadFile, dirName);
+    }
+
+    private String upload(File uploadFile, String dirName) {
+        System.out.println("private upload");
+        String fileName = dirName + "/" + changedImageName(uploadFile.getName());
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile); // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+
+        return uploadImageUrl; // 업로드된 파일의 S3 URL 주소 반환
+    }
+
+    // 실질적인 s3 업로드 부분
+    private String putS3(File uploadFile, String fileName) {
+        System.out.println("putS3");
+        amazonS3.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead) // PublicRead 권한으로 업로드
+        );
+        System.out.println("Done");
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        System.out.println("convert");
+        File convertFile = new File(file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+
+    // 랜덤 파일 이름 메서드 (파일 이름 중복 방지)
+    private String changedImageName(String originName) {
+        String random = UUID.randomUUID().toString();
+        return random + originName;
+    }
+}
