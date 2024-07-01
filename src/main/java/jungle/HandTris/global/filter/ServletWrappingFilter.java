b50afd26e343047402ad03bdf05350f2aea8ad8e
seonghoo1217 <lsh6451217@gmail.com>
2024-07-01 23:46:01 +0900

    #75 feat : MDC Filter Non Filtered White List 추가

--- src/main/java/jungle/HandTris/global/filter/ServletWrappingFilter.java
+++ src/main/java/jungle/HandTris/global/filter/ServletWrappingFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jungle.HandTris.global.config.log.CachedBodyRequestWrapper;
+import jungle.HandTris.global.dto.WhiteListURI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +16,13 @@ public class ServletWrappingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        for (String whiteListUri : WhiteListURI.WhiteListURI) {
+            if (request.getRequestURI().contains(whiteListUri)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
+
         CachedBodyRequestWrapper cachedBodyRequestWrapper = new CachedBodyRequestWrapper(request);
         filterChain.doFilter(cachedBodyRequestWrapper, response);
     }
